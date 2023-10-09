b2a158c748b0d8ee7eecc99c04c4e092c6ccdbe6
MarkoBitencCode <marko.scena@gmail.com>
2023-10-09 20:17:08 +0200

    testing manual prod env

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className='text-xl font-bold text-red-500'>Hello Codelivery</h1>
+      <h1 className='text-xl font-bold text-red-500'>
+        Hello Codelivery production testing
+      </h1>
     </>
   );
 }
